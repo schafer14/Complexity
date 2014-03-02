238ec0603061faf0e1e90192ad4f28f1047664ea
Banner <banner.schafer@gmail.com>
2014-03-02 09:23:00 +0800

    tests for connection on a variety of G(n, p) shows .05 is p limit for connectivity :0

--- RandomGraph.py
+++ RandomGraph.py
@@ -12,26 +12,24 @@ class RandomGraph(Graph):
 					e = Edge(n1, n2)
 					this.add_edge(e)
 
+def test(n, p):
+	nodes = []
+
+	for x in range(0, n):
+		n = Node(x)
+		nodes.append(n)
+
+	g = RandomGraph(nodes, [])
+	g.add_random_edges(p)
+
+	return g.is_connected()
 
 def main(script, *args):
-    v = Node('v')
-    w = Node('w')
-    x = Node('x')
-    y = Node('y')
-    z = Node('z')
-    a = Node('a')
-    b = Node('b')
-    c = Node('c')
-    d = Node('d')
-    e = Node('e')
-
-    e = Edge(v, w)
-    e1 = Edge(w, x)
-
-    g = RandomGraph([v,w,x,y,z,a,b,c,d,e], [])
-    
-    g.add_random_edges(.5)
-    print len(g.edges())
+	for n in range(1, 100):
+		for t in range(1, 100):
+			p = float(t / 100.0)
+			if not test(n, p):
+				print '(' + `n` + ', ' + `p` + ')'
 
 if __name__ == '__main__':
     import sys
